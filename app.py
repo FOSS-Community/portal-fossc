dc3e035125f28234daad662607ad24f42c543cf0
Kanishk Pachauri <krishna03012003@icloud.com>
2022-06-04 15:47:56 +0530

    added templates

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def helloworld():
     return "<center> <h1> Welcome to FOSSC Portal </h1> </center>"
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
